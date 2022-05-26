d586008d56788624ceb63b08d291ec77df8556a5
ThiiD <thiago.saber@engenharia.ufjf.br>
2022-05-26 00:29:03 -0300

    Projeto do controlador via pidTuner, matlab
    Projeto do controlador utilizando a ferramenta pidTunner, do matlab. Já foram obtidos os valores dos parametros do controlador, basta implementar em software.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+float y, e, r;
+//  y é a velocidade saída do motor (pegar pelo encoder)
+// e é o erro (saida do sistema menos a entrada)
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.println("Digite um valor de referencia: ");
+  r = Serial.read();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  e = y - r;
+
+
 }
