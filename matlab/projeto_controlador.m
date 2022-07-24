6b5b5006ecb6bc64cc2ef96a513cdd1c46a674f7
ThiiD <thiago.saber@engenharia.ufjf.br>
2022-07-24 18:48:13 -0300

    Inicio do controle
    Controle no simulink

--- matlab/projeto_controlador.m
+++ matlab/projeto_controlador.m
@@ -1,14 +1,19 @@
 clc, close all, clear all
+
+% ---------------------------------------------------------------
+% ------------------ Projeto dos controladores ------------------
+% ---------------------------------------------------------------
+%{
 T = 0.05; % Tempo de discretização
 num = [85.12];
 den = [1 20.05 100.5];
 
 % {
 % Controlador de velocidade
-G_vel = tf(num,den)
+G_vel = tf(num,den);
 G_vel_discrete = c2d(G_vel, T);
--
-%}
+pidTuner(G_vel);
+% }
 
 % {
 % Controlador de posição
@@ -19,4 +24,8 @@ G_int = tf(num_int, den_int);
 G_pos = series(G, G_int);
 G_pos_discrete = c2d(G_pos, T);
 pidTuner(G_pos);
+% }
+
+C_vel_discrete = c2d(C_vel, T);
+C_pos_discrete = c2d(C_pos, T);
 %}
